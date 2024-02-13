d2bd3193be1c2b480bad9eae19ec108810c8ad31
Jeremias <jnahuel46@gmail.com>
2024-02-13 04:08:00 -0300

    use client component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,9 +1,24 @@
-const Navbar = () => {
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
+import { ActiveLink } from "..";
+
+const navItems = [
+  { path: "/about", text: "About" },
+  { path: "/contact", text: "Contact" },
+  { path: "/pricing", text: "Pricing" },
+];
+
+export const Navbar = () => {
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link className="flex items-center" href="/">
+        <HomeIcon className="mr-2" />
+        Home
+      </Link>
+      <div className="flex flex-1"></div>
+      {navItems.map(({ path, text }) => {
+        return <ActiveLink key={text} path={path} text={text} />;
+      })}
     </nav>
   );
 };
-
-export default Navbar;
